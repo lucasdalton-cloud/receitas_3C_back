0154c35c1f39ee59cf668d872d9301df4bf951b8
LUCAS PATRICK BRUSTOLIN DALTON <lucas_dalton@estudante.sesisenai.org.br>
2026-03-04 09:08:30 -0300

    Mensagem só para falar a alteração

--- banco.sql
+++ banco.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS USUARIOS (
-	id SERIAL primary key,
-	nome VARCHAR(255) not null,
-	ativo BOOLEAN default true,
-	senha VARCHAR(255) not null,
-	criado_em TIMESTAMP default current_timestamp
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    ativo BOOLEAN DEFAULT true,
+    senha VARCHAR(255) NOT NULL,
+    criado_em TIMESTAMP DEFAULT current_timestamp
 );
-  
+
 CREATE TABLE IF NOT EXISTS RECEITAS (
-	id SERIAL primary key,
-	nome VARCHAR(255) not null,
-	ingredientes TEXT not full,
-	instrucoes TEXT not full,
-	tempo_preparo_minutos INTEGER not null,
-	usuario_id INTEGER not full references USUARIO(id) on delete cascade
-)
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    ingredientes TEXT NOT NULL,
+    instrucoes TEXT NOT NULL,
+    tempo_preparo_minutos INTEGER NOT NULL,
+    usuario_id INTEGER NOT NULL REFERENCES USUARIOS(id) ON DELETE CASCADE
+);
--- serve.js
+++ serve.js
@@ -1,11 +1,35 @@
- import Fastify from 'fastify'
+import Fastify from 'fastify'
+    import { Pool } from 'pg'
 
- const servidor = Fastify()
+    const sql = new Pool({
+        user: "postgres",
+        password: "senai",
+        host: "localhost",
+        port: 5432,
+        database: "receitas"
+})
 
- servidor.get('/usuarios', () => {
-    return 'Está funcionando'
- })
+const servidor = Fastify()
+
+servidor.get('/usuarios', async () => {
+    const resultado = await sql.query('select * from usuarios')
+    return resultado.rows
+})
+
+servidor.post('/usuarios', async (request, reply) =>{
+    const nome = request.body.nome
+    const senha = request.body.senha
+    const resultado = await sql.query('INSERT INTO usuarios (nome, senha) VALUES ($1, $2)',[nome, senha])
+    return 'usuario cadastrado!'
+})
+
+servidor.put('/usuarios/:id', async (request, reply) => {
+    const body = request.body;
+    const id = request.params.id
+    const resultado = await sql.query('update usuarios SET nome = $1, senha = $2 where id = $3', [body.nome, body.senha,id])
+    return 'usuario alterado'
+})
 
 servidor.listen({
-    port: 3000
+    port:3000
 })
